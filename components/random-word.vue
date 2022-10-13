35986352ae5a4f296586771bcc937f829d374c2b
Aleks <aleksandar.trpkovski@gmail.com>
2022-10-13 22:07:36 +1100

    Added new component for generating a random words

---
+++ components/random-word.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>{{ word }}</h1>
+        <button @click="generateRandomWord">Generate a Word</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import randomWords from "random-words";
+
+const word = ref("");
+
+function generateRandomWord() {
+    word.value = randomWords();
+}
+
+const msg = ref("");
+</script>
+
+<style lang="scss" scoped></style>
